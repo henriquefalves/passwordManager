8d0ead3252fb9fef176a710525501d27d1935be5
hfalves - Henrique <hfalves94@gmail.com>
2017-03-03 18:40:08 +0000

    Testing eclipse integration with git

--- Client/src/pt/ulisboa/tecnico/meic/sec/client/Client.java
+++ Client/src/pt/ulisboa/tecnico/meic/sec/client/Client.java
@@ -3,5 +3,7 @@ package pt.ulisboa.tecnico.meic.sec.client;
 import pt.ulisboa.tecnico.meic.sec.commoninterface.ClientAPI;
 
 public class Client implements ClientAPI {
-
+public static void main(String[] args) {
+	
+}
 }
