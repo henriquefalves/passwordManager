9dc8b3e590587163924bece827109bc29679629e
S-4-S <z4swlf@gmail.com>
2017-03-07 00:24:52 +0000

    add pom to projects

---
+++ Client/src/main/java/pt/ulisboa/tecnico/meic/sec/client/Client.java
@@ -0,0 +1,10 @@
+package pt.ulisboa.tecnico.meic.sec.client;
+
+import pt.ulisboa.tecnico.meic.sec.commoninterface.ClientAPI;
+
+public class Client implements ClientAPI {
+
+public static void main(String[] args) {
+	System.out.println("SUCCESS");
+}
+}
