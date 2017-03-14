092c4f29f5c387872207b55f0d2c7ada56e81aeb
hfalves - Henrique <hfalves94@gmail.com>
2017-03-14 14:47:02 +0000

    Added crypto functions and tests

--- CommonInterfaces/src/main/java/pt/ulisboa/tecnico/meic/sec/commoninterface/Crypto.java
+++ CommonInterfaces/src/main/java/pt/ulisboa/tecnico/meic/sec/commoninterface/Crypto.java
@@ -1,27 +1,39 @@
 package pt.ulisboa.tecnico.meic.sec.commoninterface;
 
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.Signature;
 import java.security.SignatureException;
+import java.security.spec.InvalidKeySpecException;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
 
 public class Crypto {
 
 
-	private static final String DEFAULT_HASH_ALGORITHM = "SHA-256";
-	private static final String DEFAULT_SIGN_ALGORITHM = "SHA256withRSA";
-	private static final String CIPHER_ALGORITHM1= "RSA/ECB/PKCS1Padding";
-	private static final String CIPHER_ALGORITHM2="RSA/ECB/OAEPWithSHA-1AndMGF1Padding";
-	private static final String CIPHER_ALGORITHM3="RSA/ECB/OAEPWithSHA-256AndMGF1Padding"; 
+	public static final String DEFAULT_HASH_ALGORITHM = "SHA-256";
+	public static final String DEFAULT_SIGN_ALGORITHM = "SHA256withRSA";
+	public static final String ASYMETRIC_CIPHER_ALGORITHM1= "RSA/ECB/PKCS1Padding";
+	public static final String ASYMETRIC_CIPHER_ALGORITHM2="RSA/ECB/OAEPWithSHA-1AndMGF1Padding";
+	public static final String ASYMETRIC_CIPHER_ALGORITHM3="RSA/ECB/OAEPWithSHA-256AndMGF1Padding";
+	public static final String SYMETRIC_CIPHER_ALGORITHM="AES/CBC/PKCS5Padding";
+	public static final String SYMETRIC_CIPHER_ALGORITHM2="AES/CTR/NoPadding";	
+
 	public byte[] hashData(byte[] data) {
 		MessageDigest md = null;
 		try {
@@ -84,10 +96,10 @@ public class Crypto {
 
 	}
 
-	public byte[] encrypt(byte[] data, Key key) {
+	public byte[] encrypt(byte[] data, Key key,String algorithm) {
 		Cipher rsa = null;
 			try {
-				rsa = Cipher.getInstance("CIPHER_ALGORITHM1");
+				rsa = Cipher.getInstance(algorithm);
 			} catch (NoSuchAlgorithmException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -114,10 +126,10 @@ public class Crypto {
 		return null;
 	}
 	
-	public byte[] decrypt(byte[] ciphertext, Key key) {
+	public byte[] decrypt(byte[] ciphertext, Key key, String algorithm) {
 			Cipher rsa = null;
 			try {
-				rsa = Cipher.getInstance("CIPHER_ALGORITHM1");
+				rsa = Cipher.getInstance(algorithm);
 			} catch (NoSuchAlgorithmException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -143,4 +155,118 @@ public class Crypto {
 		return null;
 	}
 	
+	public byte[] encryptSymetrickey(byte[] data, Key key,String algorithm, IvParameterSpec iv) {
+		Cipher rsa = null;
+			try {
+				rsa = Cipher.getInstance(algorithm);
+			} catch (NoSuchAlgorithmException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (NoSuchPaddingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			try {
+				rsa.init(Cipher.ENCRYPT_MODE, key, iv);
+			} catch (InvalidKeyException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InvalidAlgorithmParameterException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			try {
+				return rsa.doFinal(data);
+			} catch (IllegalBlockSizeException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (BadPaddingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+		return null;
+	}
+	
+	public byte[] decryptSymetricKey(byte[] ciphertext, Key key, String algorithm,IvParameterSpec iv) {
+			Cipher rsa = null;
+			try {
+				rsa = Cipher.getInstance(algorithm);
+			} catch (NoSuchAlgorithmException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (NoSuchPaddingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			try {
+				rsa.init(Cipher.DECRYPT_MODE, key);
+			} catch (InvalidKeyException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			try {
+				return rsa.doFinal(ciphertext);
+			} catch (IllegalBlockSizeException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (BadPaddingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		return null;
+	}
+	
+	
+	public  byte[] keyDerivationFunction( final char[] password, final byte[] salt ) {
+
+			SecretKeyFactory skf = null;
+			try {
+				skf = SecretKeyFactory.getInstance( "PBKDF2WithHmacSHA512" );
+			} catch (NoSuchAlgorithmException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			int keyLength = 256;
+			int iterations=1000;
+			int saltLength=32;
+			PBEKeySpec spec = new PBEKeySpec( password, salt, iterations, keyLength  );
+			SecretKey key = null;
+			try {
+				key = skf.generateSecret( spec );
+			} catch (InvalidKeySpecException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			byte[] res = key.getEncoded( );
+			return res;
+
+	
+	}
+
+	public static KeyPair generateKeyPairRSA2048(){
+		 KeyPairGenerator keyGen = null;
+		try {
+			keyGen = KeyPairGenerator.getInstance("RSA");
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		    keyGen.initialize(2048);
+		    KeyPair keypair = keyGen.genKeyPair();
+		    return keypair;
+	}
+	
+	public static SecretKey generateSecretKeyAES128(){
+	KeyGenerator keyGen = null;
+	try {
+		keyGen = KeyGenerator.getInstance("AES");
+	} catch (NoSuchAlgorithmException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	keyGen.init(128); // for example
+	SecretKey secretKey = keyGen.generateKey();
+	return secretKey;
+	}
 }
